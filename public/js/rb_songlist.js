abd6aec0e36528d14e0bb549ca3f46bf9681714d
Dan Book <grinnz@grinnz.com>
2017-07-15 19:21:21 -0400

    better display of requested-by information

--- public/js/rb_songlist.js
+++ public/js/rb_songlist.js
@@ -1,38 +1,3 @@
-var search_data = { search_songlist_results: [], search_for_queue: null };
-var search_vm = new Vue({
-  el: '#search_songlist',
-  data: search_data,
-  methods: {
-    search_songlist: function (event) {
-      $.getJSON('/api/songs/search', { query: $('#search_songlist_query').val() })
-        .done(function (results) {
-          search_data.search_songlist_results = results;
-        })
-    },
-    clear_search_songlist: function (event) {
-      search_data.search_songlist_results = [];
-    },
-    queue_song: function (song_id) {
-      $.post('/api/queue/add', { song_id: song_id })
-        .done(function () {
-          queue_vm.refresh_queue();
-        })
-    },
-    clear_search_for_queue: function () {
-      search_data.search_for_queue = null;
-    },
-    set_queued_song: function (position, song_id) {
-      if (position) {
-        search_vm.clear_search_for_queue();
-        $.post('/api/queue/' + position, { song_id: song_id })
-          .done(function () {
-            queue_vm.refresh_queue();
-          })
-      }
-    }
-  }
-});
-
 var queue_data = { queue_first: null, queue_remaining: [] };
 var queue_vm = new Vue({
   el: '#request_queue',
@@ -65,6 +30,41 @@ var queue_vm = new Vue({
     },
     set_search_for_queue: function (position) {
       search_data.search_for_queue = position;
+    }
+  }
+});
+
+var search_data = { search_songlist_results: [], search_for_queue: null };
+var search_vm = new Vue({
+  el: '#search_songlist',
+  data: search_data,
+  methods: {
+    search_songlist: function (event) {
+      $.getJSON('/api/songs/search', { query: $('#search_songlist_query').val() })
+        .done(function (results) {
+          search_data.search_songlist_results = results;
+        })
+    },
+    clear_search_songlist: function (event) {
+      search_data.search_songlist_results = [];
+    },
+    queue_song: function (song_id) {
+      $.post('/api/queue/add', { song_id: song_id })
+        .done(function () {
+          queue_vm.refresh_queue();
+        })
+    },
+    clear_search_for_queue: function () {
+      search_data.search_for_queue = null;
+    },
+    set_queued_song: function (position, song_id) {
+      if (position) {
+        search_vm.clear_search_for_queue();
+        $.post('/api/queue/' + position, { song_id: song_id })
+          .done(function () {
+            queue_vm.refresh_queue();
+          })
+      }
     }
   }
 });
