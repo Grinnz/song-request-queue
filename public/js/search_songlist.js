81990836a3432b75b61837eba412f96b60d3461a
Dan Book <grinnz@grinnz.com>
2017-07-15 21:16:25 -0400

    delete songs

--- public/js/search_songlist.js
+++ public/js/search_songlist.js
@@ -1,4 +1,4 @@
-var search_data = { search_songlist_results: [], search_for_queue: null };
+var search_data = { search_songlist_results: [], search_for_queue: null, confirming_delete_song: null };
 var search_vm = new Vue({
   el: '#search_songlist',
   data: search_data,
@@ -27,6 +27,21 @@ var search_vm = new Vue({
         $.post('/api/queue/' + position, { song_id: song_id })
           .done(function () {
             queue_vm.refresh_queue();
+          })
+      }
+    },
+    confirm_delete_song: function (song_id) {
+      search_data.confirming_delete_song = song_id;
+    },
+    unconfirm_delete_song: function (song_id) {
+      search_data.confirming_delete_song = null;
+    },
+    delete_song: function (song_id) {
+      if (song_id) {
+        search_vm.unconfirm_delete_song(song_id);
+        $.ajax({ url: '/api/songs/' + song_id, method: 'DELETE' })
+          .done(function () {
+            search_vm.search_songlist();
           })
       }
     }
