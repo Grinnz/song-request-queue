e4c1a403e2367035ed07a8bee72ea991efb511fd
Dan Book <grinnz@grinnz.com>
2017-07-17 21:58:33 -0400

    prevent breaking the search with single quotes

--- rb_songlist.pl
+++ rb_songlist.pl
@@ -50,8 +50,8 @@ helper valid_bot_key => sub ($c, $bot_key) {
 };
 
 helper search_songs => sub ($c, $search) {
-  my $and_search = join ' & ', map { "'$_':*" } split ' ', $search;
-  my $or_search = join ' | ', map { "'$_':*" } split ' ', $search;
+  my $and_search = join ' & ', map { "'$_':*" } map { quotemeta } split ' ', $search;
+  my $or_search = join ' | ', map { "'$_':*" } map { quotemeta } split ' ', $search;
   my $query = <<'EOQ';
 SELECT *,
 ts_rank_cd(songtext, to_tsquery('english_nostop', $1)) AS "rank"
